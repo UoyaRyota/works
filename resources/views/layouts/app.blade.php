5efd30aa455662d6b856215a7fdc9c794b99585a
uoyaryota <lestwinsdance8@gmail.com>
2023-03-20 19:51:44 +0900

    add stylesheet search footer menu provisional measures

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,83 +17,134 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <script src="https://ajaxzip3.github.io/ajaxzip3.js"></script>
 
 </head>
+
+<style>
+    .search-form-009 {
+    display: flex;
+    align-items: center;
+    overflow: hidden;
+    border: 1px solid #dfe1e5;
+    border-radius: 24px;
+    margin: 20px;
+    /* position: fixed; */
+}
+
+.search-form-009:hover {
+    box-shadow: 0 1px 6px rgb(32 33 36 / 28%);
+}
+
+.search-form-009::before {
+    width: 45px;
+    height: 15px;
+    background-image: url('data:image/svg+xml;charset=utf8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2024%2024%22%3E%20%3Cpath%20d%3D%22M23.7%2020.8%2019%2016.1c-.2-.2-.5-.3-.8-.3h-.8c1.3-1.7%202-3.7%202-6C19.5%204.4%2015.1%200%209.7%200S0%204.4%200%209.7s4.4%209.7%209.7%209.7c2.3%200%204.3-.8%206-2v.8c0%20.3.1.6.3.8l4.7%204.7c.4.4%201.2.4%201.6%200l1.3-1.3c.5-.5.5-1.2.1-1.6zm-14-5.1c-3.3%200-6-2.7-6-6s2.7-6%206-6%206%202.7%206%206-2.6%206-6%206z%22%20fill%3D%22%239aa0a6%22%3E%3C%2Fpath%3E%20%3C%2Fsvg%3E');
+    background-position: center;
+    background-repeat: no-repeat;
+    content: '';
+}
+
+header {
+    height: 50px;
+}
+
+.search-form-009 input {
+    width: 250px;
+    height: 40px;
+    padding: 5px 25px 5px 0;
+    border: none;
+    box-sizing: border-box;
+    outline: none;
+}
+
+#sp-fixed-menu{
+    position: sticky;
+    width: 100%;
+    bottom: 0px;
+    font-size: 0;
+    opacity: 1;
+    z-index: 999 !important;
+    background: #fff;
+}
+
+#sp-fixed-menu ul{
+    border-top: 1px solid #DBDBDB;
+    display: flex;
+    list-style: none;
+    padding:0;
+    margin:0;
+    width:100%;
+}
+
+#sp-fixed-menu li{
+    justify-content: center;
+    align-items: center;
+    width: 50%;
+    padding:0;
+    margin:0;
+    font-size: 14px;
+    border-right: 1px solid #fff;
+}
+
+#sp-fixed-menu li a{
+    color: black;
+    text-align: center;
+    display:block;
+    width: 100%;
+    padding:20px;
+}
+
+footer{
+    height: 100px;
+    opacity: 1;
+}
+
+.footer-icon-color{
+    color: #699BF7;
+}
+
+#sp-fixed-menu li.test a  {
+    padding-top: 10px !important;
+}
+
+
+
+</style>
 <body>
+    
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                @if ( request()->is('*users/show*') )
-        
-                @else
-                <form method="POST" action="{{route('clinicals.search')}}" class="search_container">
-                    {{csrf_field()}}
-                    <div class="form-group">
-                        <input type="search" class="form-control mr-sm-2" name="search"  value="" placeholder="キーワード" aria-label="検索...">
-                    </div>
-                        <input type="submit" value="検索" class="btn btn-info">
-                </form>
-                @endif
+                    @if ( request()->is('*clinicals*') || request()->is('*reservations*') )
+                    <form method="POST" action="{{route('clinicals.search')}}" class="search-form-009">
+                        {{csrf_field()}}
+                        <label>
+                            <input type="search" class="" name="search"  value="" placeholder="search" aria-label="検索...">
+                            
+                        </label>
+                    </form>
+                    @else
+                    
+                    @endif
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
 
         <main class="py-4">
             @yield('content')
         </main>
     </div>
+    <div id="sp-fixed-menu" class="for-sp">
+                    <ul>
+                    <li><a href=""><i class="fa-solid fa-house fa-2x footer-icon-color"></i></a></li>
+                    <li><a href="{{route('clinicals.index')}}"><i class="fa-solid fa-magnifying-glass fa-2x footer-icon-color"></i></a></li>
+                    <li class="test" ><a href="{{route('clinicals.index')}}"><i class="fa-solid fa-circle-plus fa-3x footer-icon-color"></i></i></a></li>
+                    <li><a href="{{route('reservations.index')}}"><i class="fa-regular fa-clipboard fa-2x footer-icon-color"></i></a></li>
+                    <li><a href="{{route('reservations.index')}}"><i class="fa-solid fa-list fa-2x footer-icon-color"></i></a></li>
+                    </ul>
+                </div>
+
 </body>
 </html>
